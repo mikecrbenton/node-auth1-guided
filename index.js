54e67a5a41adc48e798aa5ff8a4220292fda628c
Jason Maurer <jason@maur.co>
2020-10-27 19:38:05 -0600

    Code from guided lecture

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
+const session = require("express-session")
+const KnexSessionStore = require("connect-session-knex")(session)
+const db = require("./database/config")
 const usersRouter = require("./users/users-router")
 
 const server = express()
@@ -9,6 +12,16 @@ const port = process.env.PORT || 5000
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+server.use(session({
+	resave: false, // avoid creating sessions that have not changed
+	saveUninitialized: false, // GDPR laws against setting cookies automatically
+	secret: "keep it secret, keep it safe", // used to cryptographically sign the cookie
+	// store the session data in the database rather than in memory
+	store: new KnexSessionStore({
+		knex: db, // configured instance of knex
+		createtable: true, // if the table does not exist, it will create it automatically
+	}),
+}))
 
 server.use(usersRouter)
 
--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const bcrypt = require("bcryptjs")
 const Users = require("./users-model")
+const { restrict } = require("./users-middleware")
 
 const router = express.Router()
 
-router.get("/users", async (req, res, next) => {
+router.get("/users", restrict(), async (req, res, next) => {
 	try {
 		res.json(await Users.find())
 	} catch(err) {
@@ -24,7 +26,8 @@ router.post("/users", async (req, res, next) => {
 
 		const newUser = await Users.add({
 			username,
-			password,
+			// hash the password with a time complexity of 14
+			password: await bcrypt.hash(password, 14),
 		})
 
 		res.status(201).json(newUser)
@@ -44,10 +47,37 @@ router.post("/login", async (req, res, next) => {
 			})
 		}
 
+		// will be true or false depending on whether the password matched the hash
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+		// creates a new session and sends it back to the client
+		req.session.user = user
+
 		res.json({
 			message: `Welcome ${user.username}!`,
 		})
 	} catch(err) {
+		next(err)
+	}
+})
+
+router.get("/logout", async (req, res, next) => {
+	try {
+		// deletes the session on the server-side, so the user is no longer authenticated
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
 		next(err)
 	}
 })
